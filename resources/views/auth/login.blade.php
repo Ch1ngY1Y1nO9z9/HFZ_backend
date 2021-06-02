b938e16b268452f53889265d655ee8a317cc1158
l123050891 <l123050891@gmail.com>
2021-06-03 01:44:09 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,16 +24,16 @@
             </div>
             <div class="card fat">
                 <div class="card-body">
-                    <h4 class="card-title">登入</h4>
+                    <h4 class="card-title">LOGIN</h4>
                     <form method="POST" class="my-login-validation" novalidate="">
                         @csrf
                         <div class="form-group">
-                            <label for="email">使用者名稱</label>
+                            <label for="email">Account</label>
                             <input id="email" type="text" class="form-control" name="email" required >
                         </div>
 
                         <div class="form-group">
-                            <label for="password">密碼</label>
+                            <label for="password">Password</label>
                             <div style="position:relative" id="eye-password-0"><input id="password" type="password" class="form-control" name="password" required style="padding-right: 60px;">
                         </div>
 
